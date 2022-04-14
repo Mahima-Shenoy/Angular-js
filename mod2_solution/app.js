d60905cab74dfa6c8aa65601d30622413780724c
Mahima Shenoy <mahimashenoy8@gmail.com>
2022-04-14 18:34:12 +0530

    Module2

---
+++ mod2_solution/app.js
@@ -0,0 +1,59 @@
+(function() {
+  "use strict";
+
+  angular.module("ShoppingListCheckOff", [])
+    .controller("ToBuyShoppingController", ToBuyShoppingController)
+    .controller("AlreadyBoughtShoppingController", AlreadyBoughtShoppingController)
+    .service("ShoppingListCheckOffService", ShoppingListCheckOffService);
+
+
+  ToBuyShoppingController.$inject = ["ShoppingListCheckOffService"];
+  function ToBuyShoppingController(ShoppingListCheckOffService) {
+    var list = this;
+
+    list.items = ShoppingListCheckOffService.getItemsToBuy();
+
+    list.checkOff = function(itemIndex) {
+      console.log("Checking off item ", itemIndex);
+      ShoppingListCheckOffService.checkOff(itemIndex);
+    };
+  }
+
+
+  AlreadyBoughtShoppingController.$inject = ["ShoppingListCheckOffService"];
+  function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+    var list = this;
+
+    list.items = ShoppingListCheckOffService.getItemsBought();
+  }
+
+
+  // ShoppingListCheckOffService.$inject = [];
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    var itemsToBuy = [
+      { name: "ORANGE", quantity: 43 },
+      { name: "APPLES", quantity: 32 },
+      { name: "GRAPES", quantity: 20 },
+      { name: "BERRIES", quantity: 41 },
+      { name: "PAPAYAS", quantity: 54 },
+      { name: "MANGOES", quantity: 61 },
+    ];
+
+    var itemsBought = [];
+
+    service.getItemsToBuy = function () {
+      return itemsToBuy;
+    };
+
+    service.getItemsBought = function () {
+      return itemsBought;
+    };
+
+    service.checkOff = function (itemIndex) {
+      itemsBought.push(itemsToBuy[itemIndex]);
+      itemsToBuy.splice(itemIndex, 1);
+    };
+  }
+})();
